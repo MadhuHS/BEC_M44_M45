99217a34e167f176ed083d753d9772967323c12d
Madhu <Madhu@192.168.1.4>
2021-06-03 12:49:14 +0530

    Exceptions Updated

--- BECM44_45/src/com/jspiders/exceptions/Mainclass.java
+++ BECM44_45/src/com/jspiders/exceptions/Mainclass.java
@@ -1,21 +1,42 @@
 package com.jspiders.exceptions;
 
 public class Mainclass {
-    
-	public static void test(int num1,int num2) 
+
+	public static void test1(int num1, int num2) 
 	{
-	  System.out.println("test() starts...");
-	  
-	  int res = num1/num2;
-	  System.out.println("res : "+res);
-	  
-	  System.out.println("test() ends...");
-	}
-	public static void main(String[] args) 
-	{
-       System.out.println("Program starts...");
-       test(50,0);
-       System.out.println("Program ends...");
+		System.out.println("test() starts...");
+
+		if (num2 > 0) {
+			int res = num1 / num2;
+			System.out.println("res : " + res);
+		} else {
+			System.out.println("CANNOT DIVIDE BY ZERO");
+		}
+		System.out.println("test() ends...");
 	}
 
+	public static void test2(int num1, int num2) 
+	{
+		System.out.println("test2() starts...");
+		
+        try
+        {
+        	int res = num1 / num2;
+    		System.out.println("res : " + res);	
+        }
+        catch(ArithmeticException ref)
+        {
+        	System.out.println("ArithmeticException handled");
+			System.out.println("CANNOT DIVIDE BY ZERO");
+        }
+
+		System.out.println("test2() ends...");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Program starts...");
+		test1(50,0);
+		test2(50,0);
+		System.out.println("Program ends...");
+	}
 }
