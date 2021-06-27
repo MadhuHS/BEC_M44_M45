1e86cb841b80863a019a0078060b073cf2fa5d67
Madhu <Madhu@192.168.72.67>
2021-06-27 12:38:58 +0530

    HashSet and LinkedHashSet updated

---
+++ BECM44_45/src/com/jspiders/collections/Mainclass4.java
@@ -0,0 +1,60 @@
+package com.jspiders.collections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Mainclass4
+{
+	public static void main(String[] args)
+	{
+       //datatype arrayname = new datatype[size];
+		
+		//Create
+		HashSet<String> hs1 = new HashSet<String>();
+		
+		hs1.add("java");//insert
+		hs1.add("python");//insert
+		hs1.add("javascript");//insert
+		hs1.add("python");//insert
+		hs1.add("c++");//insert
+		
+		//read all elements->for-each
+		for(String str: hs1)
+		{
+		  System.out.println(str);
+		}
+		
+		System.out.println();
+		
+		//read all elements->Iterator
+	    Iterator<String>  i1 = hs1.iterator();
+	    while(i1.hasNext())
+	    {
+	    	System.out.println(i1.next());
+	    }
+	    
+	    //update
+	    boolean b1 = hs1.contains("python");
+	    
+	    if(b1 == true)
+	    {
+	    	hs1.remove("python");
+	    	hs1.add("DevOps");
+	    }
+	    
+	    System.out.println();
+	    Iterator<String>  i2 = hs1.iterator();
+	    while(i2.hasNext())
+	    {
+	    	System.out.println(i2.next());
+	    }
+
+	}
+}
+
+
+
+
+
+
+
