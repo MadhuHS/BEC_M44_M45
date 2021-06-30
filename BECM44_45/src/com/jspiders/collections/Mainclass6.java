a19a66b8550eaebb6947aac5e683e50b070fde73
Madhu <Madhu@192.168.1.4>
2021-06-30 12:34:29 +0530

    Updates Notes .  StringBuffer Sorting updated

--- BECM44_45/src/com/jspiders/collections/Mainclass6.java
+++ BECM44_45/src/com/jspiders/collections/Mainclass6.java
@@ -1,7 +1,22 @@
 package com.jspiders.collections;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
+
+
+class StringBufferComparator implements Comparator<StringBuffer>
+{
+	@Override
+	public int compare(StringBuffer sb1, StringBuffer sb2)
+	{
+		String s1 = sb1.toString();
+		String s2 = sb2.toString();
+		int res = s2.compareTo(s1);
+		return res;
+	}
+	
+}
 
 public class Mainclass6
 {
@@ -9,16 +24,17 @@ public class Mainclass6
 	{
        //datatype arrayname = new datatype[size];
 		
+		StringBufferComparator comp = new StringBufferComparator();
 		//Create
-		TreeSet<String> hs1 = new TreeSet<String>();
+		TreeSet<StringBuffer> hs1 = new TreeSet<StringBuffer>(comp);
 		
-		String s1 = new String("bat");
-		String s2 = new String("dam");
-		String s3 = new String("ant");
-		String s4 = new String("cat");
-		String s5 = new String("ele");
-		String s6 = new String("can");
-		String s7 = new String("CAT");	
+		StringBuffer s1 = new StringBuffer("bat");
+		StringBuffer s2 = new StringBuffer("dam");
+		StringBuffer s3 = new StringBuffer("ant");
+		StringBuffer s4 = new StringBuffer("cat");
+		StringBuffer s5 = new StringBuffer("ele");
+		StringBuffer s6 = new StringBuffer("can");
+		StringBuffer s7 = new StringBuffer("CAT");	
 		
 		hs1.add(s1);//insert
 		hs1.add(s2);//insert
@@ -29,7 +45,7 @@ public class Mainclass6
 		hs1.add(s7);//insert
 		
 		
-	    Iterator<String>  i1 = hs1.iterator();
+	    Iterator<StringBuffer>  i1 = hs1.iterator();
 	    while(i1.hasNext())
 	    {
 	    	System.out.println(i1.next());
